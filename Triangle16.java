19ad18e26c60fd5f5695cf63fccbbc7b8b4e33ef
zakariahaniya <zakalvha@gmail.com>
2024-10-30 07:49:43 +0700

    daspro jobsheet 8

---
+++ Triangle16.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * Triangle16
+ */
+public class Triangle16 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai N = ");
+        int N = sc.nextInt();
+        int i = 0;
+        while (i <= N) {
+            int j = 0;
+            while (j < i) {
+                System.out.print("*");
+                j++;
+            }
+            i++;
+            System.out.println("*");
+        }
+    }
+}
